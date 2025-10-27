1eea24efe5190d7b0e7f2d4dba01da78f432217d
Dharzzzz <pamaylaondarence153@gmail.com>
2025-10-27 20:24:26 +0800

    database, sp, user creation

---
+++ pages/testconnection.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'camsdatabase.php'; 
+
+$db = new Database();
+$conn = $db->getConnection();
+
+
+if ($conn) { //for testing connection only
+    echo "Database connection successful!";
+} else {
+    echo "Database connection failed.";
+}
+?>
